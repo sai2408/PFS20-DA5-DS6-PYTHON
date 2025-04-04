79a03d4ccfcd3f2659008d995c6aac4fc66cf82d
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2025-04-04 11:00:53 +0530

    Add files via upload

---
+++ Day 33 [04042025]/abc.py
@@ -0,0 +1,27 @@
+#Program - 1
+'''
+import os
+print(os.listdir())
+'''
+#Program - 2
+'''
+from os import *
+chdir('sample')
+print(listdir())
+'''
+#Program - 3
+'''
+from os import *
+mkdir("new_folder")
+'''
+#Program - 4
+'''
+from os import *
+chdir("sample")
+rmdir("sample11")
+'''
+
+
+
+
+
